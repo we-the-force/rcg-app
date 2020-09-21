572a9023d628cbbd152a76c724dc8e08ffa5cc4f
Iojioji <erfamel@hotmail.com>
2020-09-21 16:11:03 -0500

    Add 'target:_blank' to right-panel-autor

--- src/components/autores/autores-panel.jsx
+++ src/components/autores/autores-panel.jsx
@@ -23,7 +23,7 @@ export default class AutoresPanel extends Component {
                         let autorImgUrl = autor.img != null ? `http://${window.location.hostname}:1337${autor.img.url}` : ``;
                         let twLink = autor.twitter_link   != null ? (<a href={autor.twitter_link}   target="_blank"><img src="../static/icons/TW_Icon.png"/></a>) : ("");
                         let fbLink = autor.facebook_link  != null ? (<a href={autor.facebook_link}  target="_blank"><img src="../static/icons/FB_Icon.png"/></a>) : ("");
-                        let igLink = autor.instagram_link != null ? (<a href={autor.instagram_link} ><img src="../static/icons/IG_Icon.png"/></a>) : ("");
+                        let igLink = autor.instagram_link != null ? (<a href={autor.instagram_link} target="_blank"><img src="../static/icons/IG_Icon.png"/></a>) : ("");
                         console.log("autoresPanel igLink:", igLink);
                         console.log("autoresPanel igLink2:", autor.instagram_link);
                         return (<Card key={key}>
