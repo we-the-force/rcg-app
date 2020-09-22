4b4354338b44a355c85cf587b02d526997ab77f4
srlechuga18 <aledeleon222@hotmail.com>
2020-09-22 14:16:51 -0500

    footer responsive

--- src/components/general/footer.jsx
+++ src/components/general/footer.jsx
@@ -4,6 +4,11 @@ import FaceFoot from '@/static/icons/footer_face.png'
 import TwitFoot from '@/static/icons/footer_twit.png'
 import YouFoot from '@/static/icons/footer_you.png'
 import InstaFoot from '@/static/icons/footer_insta.png'
+
+import TWIcon from '@/static/icons/TW_Icon.png'
+import FBIcon from '@/static/icons/FB_Icon.png'
+import YTIcon from '@/static/icons/YT_Icon.png'
+import IGIcon from '@/static/icons/IG_Icon.png'
 import {
     Block,
     Link
@@ -17,11 +22,11 @@ export default class Footer extends Component {
     render() {
         return (
             <Block className="footer display-flex justify-content-space-between">
-                <a href="/" className="left">
+                <a href="/" className="left logo">
                     <img src={LogoNegro} alt="" />
                 </a>
-                <Block className="center">
-                    <div className="display-flex">
+                <Block className="center links">
+                    <div className="cont display-flex">
                         <Link href="/derecho_replica">Derecho de replica</Link>
                         <hr />
                         <Link href="/aviso_privacidad">Aviso de privacidad</Link>
@@ -36,19 +41,36 @@ export default class Footer extends Component {
                         <p>©2020 RCG</p>
                     </div>
                 </Block>
-                <Block className="right display-flex justify-content-space-between align-items-center">
+                <Block className="right display-flex justify-content-space-between align-items-center social-desk">
                     <a href="#" className="faceIcon display-flex justify-content-center align-items-center">
-                        <img src={FaceFoot} alt=""/>
+                        <img src={FaceFoot} alt="" />
                     </a>
                     <a href="#" className="twitIcon display-flex justify-content-center align-items-center">
-                        <img src={TwitFoot} alt=""/>
+                        <img src={TwitFoot} alt="" />
                     </a>
                     <a href="#" className="youIcon display-flex justify-content-center align-items-center">
-                        <img src={YouFoot} alt=""/>
+                        <img src={YouFoot} alt="" />
                     </a>
                     <a href="#" className="instaIcon display-flex justify-content-center align-items-center">
-                        <img src={InstaFoot} alt=""/>
+                        <img src={InstaFoot} alt="" />
                     </a>
+                </Block>
+                <Block className="display-flex justify-content-space-between align-items-center social-mobile">
+                    <p>Siguenos en:</p>
+                    <Block className="icons display-flex justify-content-space-between align-items-center ">
+                        <a href="#" className="display-flex justify-content-center align-items-center">
+                            <img src={FBIcon} alt="" srcSet="" />
+                        </a>
+                        <a href="#" className="display-flex justify-content-center align-items-center">
+                            <img src={TWIcon} alt="" srcSet="" />
+                        </a>
+                        <a href="#" className="display-flex justify-content-center align-items-center">
+                            <img src={YTIcon} alt="" srcSet="" />
+                        </a>
+                        <a href="#" className="display-flex justify-content-center align-items-center">
+                            <img src={IGIcon} alt="" srcSet="" />
+                        </a>
+                    </Block>
                 </Block>
             </Block>
         );
