e75e90ec10127c1639903712480280550b29cd3c
srlechuga18 <aledeleon222@hotmail.com>
2020-09-24 13:20:58 -0500

    animaciones a webo

--- src/components/cards_news/news_relevantes.jsx
+++ src/components/cards_news/news_relevantes.jsx
@@ -12,16 +12,18 @@ export default function NewsRelevantes(props) {
     let { className, id } = props;
     const [modal, setModal] = useState('');
     return (
-        <Block className={`NewsRel_Cont ${className}`} id={id}>
+        <Block className={`NewsRel_Cont ${className} ${modal}`} id={id}>
             <img src={TestImage} alt="" />
-            <Block className={`top-cont ${modal}`}>
+            <Block className={`top-cont`}>
                 <Link className="categoria upperscale">Locales</Link>
-                <a className="add" href="" onClick={() => { setModal(modal == '' ? 'modal-in' : '') }}><Icon material="add" /></a>
             </Block>
-            <Block className={`content ${modal}`}>
-                <p>Aqui va contenido cuado se despliegue</p>
+            <Block className={`add`} href="#">
+                <Link iconMaterial="add" onClick={() => { setModal(modal == '' ? 'modal-in' : '') }}/>
+                <Block className={`content`}>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec feugiat mauris et tellus faucibus bibendum. Ut egestas aliquam facilisis. Aliquam tristique libero ut sapien consequat imperdiet. Sed ut auctor dolor. In iaculis ligula eu nisl laoreet, quis suscipit urna gravida. Ut nunc elit, imperdiet nec lorem commodo, lacinia mattis libero. Pellentesque fringilla purus nunc, ut semper nibh placerat ut. In euismod fermentum eros. Mauris nec sodales dui.</p>
+                </Block>
             </Block>
-            <Block className={`bottom-cont ${modal}`}>
+            <Block className={`bottom-cont`}>
                 <Link className="title" href='#'>Aqui va el titulo mas grande para probar cuanto cabe aqui</Link>
                 <BlockFooter>
                     <p className="fecha">10 septiembre</p>
