b00d43ec89a2a0dbbefe767905efd1276bfb90d6
srlechuga18 <aledeleon222@hotmail.com>
2020-09-25 14:07:12 -0500

    search cards myd

--- src/components/cards_news/news_busqueda.jsx
+++ src/components/cards_news/news_busqueda.jsx
@@ -19,11 +19,14 @@ export default function NewsBusqueda(props) {
                 <Block className='content'>
                     <Block className='info'>
                         <Link className="autor">Nombre Reporter@</Link>&nbsp;-&nbsp;
-                    <p className="fecha">10 septiembre</p>
+                        <p className="fecha">10 septiembre</p>
                     </Block>
                     <Link className="title" href='#'>Aqui va el titulo mas grande para probar cuanto cabe aqui</Link>
-                    <p className="preview">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec feugiat mauris et tellus faucibus bibendum. Ut egestas aliquam facilisis. Aliquam tristique libero ut sapien consequat imperdiet. Sed ut auctor dolor. In iaculis ligula eu nisl laoreet, quis suscipit urna gravida. Ut nunc elit, imperdiet nec lorem commodo, lacinia mattis libero. Pellentesque fringilla purus nunc, ut semper nibh placerat ut. In euismod fermentum eros. Mauris nec sodales dui.</p>
-                    <a className="more-link">Ver más</a>
+                    <p className="preview">Lorem ipsum dolor sit amet,. Ut nunc elit, imperdiet nec lorem commodo, lacinia mattis libero. Pellentesque fringilla purus nunc, ut semper nibh placerat ut. In euismod fermentum eros. Mauris nec sodales dui.Lorem ipsum dolor sit amet,. Ut nunc elit, imperdiet nec lorem commodo, lacinia mattis libero. Pellentesque fringilla purus nunc, ut semper nibh placerat ut. In euismod fermentum eros. Mauris nec sodales dui.</p>
+                    <a className="more" href="/">Ver más</a>
+                    {/* 
+                    <a className="more-link">Ver más</a> 
+                    */}
                 </Block>
             </Block>
         </Card>
