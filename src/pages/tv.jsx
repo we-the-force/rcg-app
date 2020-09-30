47c487df34ffb5236b3b34454771b4f5e527dbce
Iojioji <erfamel@hotmail.com>
2020-09-30 18:24:41 -0500

    Oops, import tablet panels to tv page

--- src/pages/tv.jsx
+++ src/pages/tv.jsx
@@ -2,6 +2,8 @@ import React, { useRef, useState, useEffect } from 'react';
 import Nav from '@/components/general/navbar/navbar';
 import LeftPanel from '@/components/general/left_panel/left-panel';
 import RightPanel from '@/components/general/right_panel/right-panel';
+import LeftPanelTablet from '@/components/general/left_panel/left-panel-tablet';
+import RightPanelTablet from '@/components/general/right_panel/right-panel-tablet';
 import Footer from '@/components/general/footer';
 import AdsTop from '@/components/general/ads_top';
 import TVPanel from '@/components/tv/tv-panel';
