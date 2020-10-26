ac8c9dce3bbf07e9fa265218930a55daef05d64b
Iojioji <erfamel@hotmail.com>
2020-10-26 17:12:21 -0600

    Add default message on Busqueda when no results are thrown

--- src/components/busqueda/busqueda-panel.jsx
+++ src/components/busqueda/busqueda-panel.jsx
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import NewsBusqueda from '@/components/cards_news/news_busqueda.jsx';
+import NotFoundPanel from '@/components/not-found-panel';
 import back_head from '@/static/imgs/card_back_6.png'
 import {
     Card,
@@ -9,21 +10,28 @@ import {
 
 export default function BusquedaPanel(props) {
     const { articulos, title } = props;
+    let content = articulos.length > 0 ? articulos.map((articulo, i) => {
+        return (
+            <NewsBusqueda key={i} className="" articulo={articulo}/>
+        )
+    }) :
+    <div className="not_found_panel">
+        <Card className="not_found_card">
+            <h2>La busqueda no trajo resultados</h2>
+            <p>intenta usando diferentes terminos</p>
+        </Card>
+    </div>;
+        
+
     return (
         <Block className="busqueda_panel center_panel">
             <Card className="new_head">
-                <CardHeader>{title}</CardHeader>
+                <CardHeader>Busqueda: "{title}"</CardHeader>
                 <div className="head_logo">
                     <img src={back_head} alt="" />
                 </div>
             </Card>
-            {
-                articulos.map((articulo, i) => {
-                    return (
-                        <NewsBusqueda key={i} className="" articulo={articulo} />
-                    );
-                })
-            }
+            {content}
         </Block>
     );
 }
