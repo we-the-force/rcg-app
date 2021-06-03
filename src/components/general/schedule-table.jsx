e1634b2482a06e5330fb4272d62e6e78a0231c26
srlechuga18 <aledleon2@gmail.com>
2021-06-03 15:48:11 -0500

    error de referencia

--- src/components/general/schedule-table.jsx
+++ src/components/general/schedule-table.jsx
@@ -46,9 +46,9 @@ export default function ScheduleTable(props) {
                     newValue.push({
                         day: prog.day,
                         inicio: moment(t).format('HH:mm'),
-                        nombre: prog.programa.Nombre,
-                        desc: prog.programa.Descripcion,
-                        url: prog.programa.cover ? prog.programa.cover.url : null
+                        nombre: prog.nombre,
+                        desc: prog.desc,
+                        url: prog.url
                     });
                 } while (t.isBefore(hora_f));
             });
