50cb7ad1bc1174f7aee7ce36d892d18333ee8843
srlechuga18 <aledleon2@gmail.com>
2021-06-07 14:01:22 -0500

    https request

--- tunel.js
+++ tunel.js
@@ -3,48 +3,19 @@ const app = express();
 const port = process.env.PORT || 5000;
 const path = require("path");
 const fs = require("fs");
-const fetch = require("node-fetch");
+const https = require('https');
 
 const URL = "https://rcgmedia.mx";
-const apiURL = "https://api.rcgmedia.mx";
-
-const query = `
-	query{
-	 articulos(where: { url: $url }) {
-		Titulo,
-		description,
-		cover{
-			url
-		}
-	}
-}
-`;
-
-var variables = {
-	url: "",
-};
+const apiURL = "localhost:1337";
 
 app.get("/articulo/:url", function (request, response) {
 	const filePath = path.resolve(__dirname, "./www", "index.html");
-	variables.url = request.params.url;
-	fetch(apiURL, {
-		method: "post",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({ query, variables }),
-	})
-		.then((response) => {
-			console.log("resp "+response.data);
-			return response.json()
-		})
-		.then((data) => {
-			console.log("data "+data);
-			return data;
-		})
-		.catch((e) => {
-			console.log("e "+e);
-		});
+	// variables.url = request.params.url;
+	https.get(apiURL + "/articulos?url=" + request.params.url, res => {
+		console.log(res);
+	  }).on('error', err => {
+		console.log('Error: ', err.message);
+	  });
 	fs.readFile(filePath, "utf8", function (err, data) {
 		if (err) {
 			return console.log(err);
