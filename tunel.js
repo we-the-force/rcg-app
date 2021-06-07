6567e67963f5cf2cd60109b46a2b13469e05d21c
srlechuga18 <aledleon2@gmail.com>
2021-06-07 13:16:29 -0500

    test to get article

--- tunel.js
+++ tunel.js
@@ -3,43 +3,48 @@ const app = express();
 const port = process.env.PORT || 5000;
 const path = require("path");
 const fs = require("fs");
+const fetch = require("node-fetch");
 
-// app.get("/", function (request, response) {
-// 	const filePath = path.resolve(__dirname, "./www", "index.html");
-
-// 	// read in the index.html file
-// 	fs.readFile(filePath, "utf8", function (err, data) {
-// 		if (err) {
-// 			return console.log(err);
-// 		}
-
-// 		// replace the special strings with server generated strings
-// 		data = data.replace(/\$OG_TITLE/g, "Home Page");
-// 		data = data.replace(/\$OG_DESCRIPTION/g, "Home page description");
-// 		result = data.replace(/\$OG_IMAGE/g, "https://i.imgur.com/V7irMl8.png");
-// 		response.send(result);
-// 	});
-// });
+const URL = "https://rcgmedia.mx"
+const apiURL = "https://api.rcgmedia.mx/graphql"
+const query = `
+query{
+		articulos(where: { url: $url}){
+		Titulo,
+		description,
+		cover{
+			url
+		}
+	} 
+}`;
 
 app.get("/articulo/:url", function (request, response) {
 	const filePath = path.resolve(__dirname, "./www", "index.html");
-	console.log(request.params);
-	fs.readFile(filePath, "utf8", function (err, data) {
-		if (err) {
-			return console.log(err);
-		}
-		data = data.replace(/\$OG_TITLE/g, "Articulo Page");
-		data = data.replace(/\$OG_DESCRIPTION/g, "Articulo description");
-		result = data.replace(/\$OG_IMAGE/g, "https://i.imgur.com/V7irMl8.png");
-		response.send(result);
+	console.log(request.params.url);
+	fetch(apiURL,{
+		method: "POST",
+		body: JSON.stringify({
+			query: query,
+			variables: { url: request.params.url }
+		}),
+	})
+	.then(result => {
+		return result.json();
+	})
+	.then(article => {
+		console.log("data returned:\n", article);
+		fs.readFile(filePath, "utf8", function (err, data) {
+			if (err) {
+				return console.log(err);
+			}
+			data = data.replace(/\$OG_TITLE/g, "Articulo Page");
+			data = data.replace(/\$OG_DESCRIPTION/g, "Articulo description");
+			result = data.replace(/\$OG_IMAGE/g, "https://i.imgur.com/V7irMl8.png");
+			response.send(result);
+		});
 	});
 });
 
 app.use(express.static(path.resolve(__dirname, "./www")));
-
-// app.get("*", function (request, response) {
-// 	const filePath = path.resolve(__dirname, "./www", "index.html");
-// 	response.sendFile(filePath);
-// });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
