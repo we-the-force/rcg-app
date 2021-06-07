493fa3ec8a390e90b01032fc85020b2a34632892
srlechuga18 <aledleon2@gmail.com>
2021-06-07 13:46:32 -0500

    test fecth

--- tunel.js
+++ tunel.js
@@ -1,50 +1,58 @@
-var express = require("express"); // Get the module
+const express = require("express"); // Get the module
 const app = express();
 const port = process.env.PORT || 5000;
 const path = require("path");
 const fs = require("fs");
 const fetch = require("node-fetch");
 
-const URL = "https://rcgmedia.mx"
-const apiURL = "https://api.rcgmedia.mx/graphql"
+const URL = "https://rcgmedia.mx";
+const apiURL = "https://api.rcgmedia.mx";
+
 const query = `
-query{
-		articulos(where: { url: $url}){
+	query articulos(where: { url: $url }) {
 		Titulo,
 		description,
 		cover{
 			url
 		}
-	} 
-}`;
+	}
+`;
+
+var variables = {
+	url: "",
+};
 
 app.get("/articulo/:url", function (request, response) {
 	const filePath = path.resolve(__dirname, "./www", "index.html");
-	console.log(request.params.url);
-	fetch(apiURL,{
-		method: "POST",
-		body: JSON.stringify({
-			query: query,
-			variables: { url: request.params.url }
-		}),
+	variables.url = request.params.url;
+	console.log("var "+variables.url);
+	fetch(apiURL, {
+		method: "post",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ query, variables }),
 	})
-	.then(result => {
-		console.log(result);
-		return result.json();
-	})
-	.then(article => {
-		console.log("data returned:\n", article);
-		fs.readFile(filePath, "utf8", function (err, data) {
-			if (err) {
-				return console.log(err);
-			}
-			data = data.replace(/\$OG_TITLE/g, "Articulo Page");
-			data = data.replace(/\$OG_DESCRIPTION/g, "Articulo description");
-			result = data.replace(/\$OG_IMAGE/g, "https://i.imgur.com/V7irMl8.png");
-			response.send(result);
+		.then((response) => {
+			console.log("resp "+response);
+			response.json()
+		})
+		.then((data) => {
+			console.log("data "+data);
+			return data;
+		})
+		.catch((e) => {
+			console.log("e "+e);
 		});
-	})
-	.catch(error => console.error(error));
+	fs.readFile(filePath, "utf8", function (err, data) {
+		if (err) {
+			return console.log(err);
+		}
+		data = data.replace(/\$OG_TITLE/g, "Articulo Page");
+		data = data.replace(/\$OG_DESCRIPTION/g, "Articulo description");
+		result = data.replace(/\$OG_IMAGE/g, "https://i.imgur.com/V7irMl8.png");
+		response.send(result);
+	});
 });
 
 app.use(express.static(path.resolve(__dirname, "./www")));
