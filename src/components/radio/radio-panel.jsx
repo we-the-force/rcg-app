7377ab9624a414f4b49607041bbba88070dedfd6
srlechuga18 <aledleon2@gmail.com>
2021-06-10 13:20:42 -0500

    traer los props upsi error mio

--- src/components/radio/radio-panel.jsx
+++ src/components/radio/radio-panel.jsx
@@ -19,7 +19,7 @@ import {
 } from 'framework7-react';
 
 export default function RadioPanel(props) {
-    const { estacion, estaciones, programacion, table_id, isOut } = props;
+    const { estacion, estaciones, programacion, table_id, isOut, playPause, setPlayPause } = props;
     const { descripcion, source_url, nombre, logo } = estacion[0];
     const [playWasTouched, setPlayWasTouched] = useState(false);
     const [sourceURL, setSourceURL] = useState(source_url);
