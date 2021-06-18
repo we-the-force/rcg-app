71b8a1f78b7570dec73575a9fb34f00e04b45a20
Will Alvarez <will.alvarez@gmail.com>
2021-06-18 14:13:15 -0500

    new plugin ads

--- src/components/general/ads/ads_top.jsx
+++ src/components/general/ads/ads_top.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Card, Block, Link } from "framework7-react";
 import uane from "../../../static/imgs/uane.gif";
 
-import AdSense from 'react-adsense';
+import GoogleAdsense from 'react-adsense-google';
 
 export default class adsTop extends Component {
 	constructor() {
@@ -17,23 +17,16 @@ export default class adsTop extends Component {
 				<Block className="left"></Block>
 				<Block className="center">
 					<Block className="ads">
-						{/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> */}
-						{/* <ins
-							className="adsbygoogle"
-							style={{display: 'block',height: '100%'}}
-							data-ad-client="ca-pub-5946382133965347"
-							data-ad-slot="7621123541"
-							data-ad-format="auto"
-							data-full-width-responsive="true"
-						></ins> */}
-						<AdSense.Google
-							client='ca-pub-9419050468962974'
-							slot='2504924643'
-							style={{ display: 'block' }}
-							format='auto'
-							responsive='true'
-							layoutKey='-gw-1+2a-9x+5c'
+						
+						<GoogleAdsense
+							adClient='ca-pub-9419050468962974'
+							adSlot='2504924643'
+							style={{'display': 'block', 'text-align': 'center'}}
+							adLayout='in-article'
+							adFormat='fluid'
+							fullWidthResponsive='true'
 						/>
+						
 						{/* <ins className="adsbygoogle"
 										style={{display: 'block'}}
 										data-ad-client="ca-pub-9419050468962974"
@@ -49,7 +42,6 @@ export default class adsTop extends Component {
 						}}/>
 						</Link>
 						
-						{/* <script>(adsbygoogle = window.adsbygoogle || []).push({});</script> */}
 					</Block>
 				</Block>
 				<Block className="right"></Block>
