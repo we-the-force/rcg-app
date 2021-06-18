ec81ca8ef37f9951928691334a05faa27888bd3d
Will Alvarez <will.alvarez@gmail.com>
2021-06-18 14:36:25 -0500

    test

--- src/components/general/ads/ads_top.jsx
+++ src/components/general/ads/ads_top.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Card, Block, Link } from "framework7-react";
 import uane from "../../../static/imgs/uane.gif";
 
-import GoogleAdsense from 'react-adsense-google';
+
 
 export default class adsTop extends Component {
 	constructor() {
@@ -11,29 +11,31 @@ export default class adsTop extends Component {
 	}
 	
 	render() {
-		
+		useEffect(() => {
+				window.adsbygoogle = window.adsbygoogle || []
+				window.adsbygoogle.push({})
+		})
 		return (
 			<Block className="ads_cont">
 				<Block className="left"></Block>
 				<Block className="center">
 					<Block className="ads">
-						
-						<GoogleAdsense
-							adClient='ca-pub-9419050468962974'
-							adSlot='2504924643'
-							style={{'display': 'block', 'text-align': 'center'}}
-							adLayout='in-article'
-							adFormat='fluid'
-							fullWidthResponsive='true'
-						/>
-						
-						{/* <ins className="adsbygoogle"
+						{/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> */}
+						{/* <ins
+							className="adsbygoogle"
+							style={{display: 'block',height: '100%'}}
+							data-ad-client="ca-pub-5946382133965347"
+							data-ad-slot="7621123541"
+							data-ad-format="auto"
+							data-full-width-responsive="true"
+						></ins> */}
+						<ins className="adsbygoogle"
 										style={{display: 'block'}}
 										data-ad-client="ca-pub-9419050468962974"
 										data-ad-slot="2504924643"
 										data-ad-format="auto"
 										data-adtest="on"
-										data-full-width-responsive="true"></ins> */}
+										data-full-width-responsive="true"></ins>
 						
 						<Link href="https://www.uane.edu.mx/campus/saltillo" target="_blank" external>
 						<img src={uane} alt="" style={{
@@ -42,6 +44,7 @@ export default class adsTop extends Component {
 						}}/>
 						</Link>
 						
+						{/* <script>(adsbygoogle = window.adsbygoogle || []).push({});</script> */}
 					</Block>
 				</Block>
 				<Block className="right"></Block>
