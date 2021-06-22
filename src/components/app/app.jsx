accc90197bb7f98de208b0b9c91bbaab99abda13
srlechuga18 <aledleon2@gmail.com>
2021-06-22 14:06:10 -0500

    me faltaron llaves

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -405,7 +405,7 @@ export default class extends React.Component {
 					OneSignal.init({
 						appId: "2b8f51fa-8098-49d8-a9a5-a36441f41907", //STEP 9
 					});
-			// 	});
+				});
 			// });
 			// 			reg.addEventListener("updatefound", () => {
 			// 				console.log("update");
