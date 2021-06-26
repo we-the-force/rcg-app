cf14db7ebb9338c2f8c29972bd7d047ff1361d7f
srlechuga18 <aledleon2@gmail.com>
2021-06-25 19:06:24 -0500

    dependency

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -14,7 +14,7 @@ import doscuatrosiete from "@/static/imgs/24-7.png";
 import { f7, f7ready } from "framework7-react";
 import { useQuery, useLazyQuery } from "@apollo/client";
 import { HomePage, CategoriaHome } from "@/graphql/queries.graphql";
-import { Page, Block, PageContent, Preloader, Navbar } from "framework7-react";
+import { Page, Block, PageContent, Preloader, Navbar, NavLeft, NavRight, } from "framework7-react";
 
 export default function Home(props) {
 	//query de la pagina
