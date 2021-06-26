a89a5b4e707c2098625c42b649611a0818007c95
Will Alvarez <will.alvarez@gmail.com>
2021-06-26 10:16:58 -0500

    http en nativo comentado

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -26,8 +26,10 @@ const errorLink = onError(({ graphQLErrors }) => {
 
 const client = new ApolloClient({
 	uri: `${process.env.PROTOCOL}://${process.env.API_HOSTNAME}/graphql`,
+	// uri: `http://${process.env.API_HOSTNAME}/graphql`,
 	cache: new InMemoryCache(),
 	link: ApolloLink.from([errorLink, new HttpLink({ uri: `${process.env.PROTOCOL}://${process.env.API_HOSTNAME}/graphql` })]),
+	// link: ApolloLink.from([errorLink, new HttpLink({ uri: `http://${process.env.API_HOSTNAME}/graphql` })]),
 });
 window.OneSignal = window.OneSignal || [];
 const OneSignal = window.OneSignal;
@@ -47,11 +49,11 @@ export default class extends React.Component {
 			name: "RCG webpage", // App name
 			theme: "auto", // Automatic theme detection
 
-			pushStateRoot: window.location.protocol + "//dev." + window.location.hostname + "",
+			// pushStateRoot: window.location.protocol + "//" + window.location.hostname + "",
 			view: {
-				pushState: true,
-				pushStateRoot: `${process.env.PROTOCOL}://${process.env.APP_HOSTNAME}`,
-				pushStateSeparator: "",
+				// pushState: true,
+				// pushStateRoot: `${process.env.PROTOCOL}://${process.env.APP_HOSTNAME}`,
+				// pushStateSeparator: "",
 			},
 
 			autoDarkTheme: true,
