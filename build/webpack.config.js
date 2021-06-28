96d62770b6e5d10349958b68e8901b2335024233
Will Alvarez <will.alvarez@gmail.com>
2021-06-28 18:03:40 -0500

    webpack

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
@@ -20,206 +19,254 @@ const isCordova = target === 'cordova';
 
 
 module.exports = {
-    mode: env,
-    entry: {
-        app: './src/js/app.js',
+  mode: env,
+  entry: {
+    app: "./src/js/app.js",
+  },
+  output: {
+    path: resolvePath(isCordova ? "cordova/www" : "www"),
+    filename: "js/[name].js",
+    chunkFilename: "js/[name].js",
+    publicPath: "",
+    hotUpdateChunkFilename: "hot/hot-update.js",
+    hotUpdateMainFilename: "hot/hot-update.json",
+    globalObject: "this",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+    alias: {
+      "@": resolvePath("src"),
     },
-    output: {
-        path: resolvePath(isCordova ? 'cordova/www' : 'www'),
-        filename: 'js/[name].js',
-        chunkFilename: 'js/[name].js',
-        publicPath: '',
-        hotUpdateChunkFilename: 'hot/hot-update.js',
-        hotUpdateMainFilename: 'hot/hot-update.json',
-        globalObject: 'this',
+  },
+  devtool: env === "production" ? "source-map" : "eval",
+  devServer: {
+    hot: true,
+    open: true,
+    compress: true,
+    contentBase: "/www/",
+    disableHostCheck: true,
+    historyApiFallback: true,
+    watchOptions: {
+      poll: 1000,
     },
-    resolve: {
-        extensions: ['.js', '.jsx', '.json'],
-        alias: {
-            '@': resolvePath('src'),
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        sourceMap: false,
+        minify: (file, sourceMap, minimizerOptions) => {
+          // The `minimizerOptions` option contains option from the `terserOptions` option
+          // You can use `minimizerOptions.myCustomOption`
+          const extractedComments = [];
+
+          // Custom logic for extract comments
+
+          const { map, code } = require("uglify-js") // Or require('./path/to/uglify-module')
+            .minify(file, {
+              /* Your options for minification */
+            });
+
+          return { map, code, extractedComments };
         },
-
-    },
-    devtool: env === 'production' ? 'source-map' : 'eval',
-    devServer: {
-        hot: true,
-        open: true,
-        compress: true,
-        contentBase: '/www/',
-        disableHostCheck: true,
-        historyApiFallback: true,
-        watchOptions: {
-            poll: 1000,
-        },
-    },
-    optimization: {
-        minimizer: [new TerserPlugin({
-            sourceMap: true,
-        })],
-    },
-    module: {
-        rules: [{
-                test: /\.(mjs|js|jsx)$/,
-                use: 'babel-loader',
-                include: [
-                    resolvePath('src'),
-                    resolvePath('node_modules/framework7'),
-
-                    resolvePath('node_modules/framework7-react'),
-
-                    resolvePath('node_modules/template7'),
-                    resolvePath('node_modules/dom7'),
-                    resolvePath('node_modules/ssr-window'),
-                ],
-            },
-
-
-
-            {
-                test: /\.css$/,
-                use: [
-                    (env === 'development' ? 'style-loader' : {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: '../'
-                        }
-                    }),
-                    'css-loader',
-                    'postcss-loader',
-                ],
-            },
-            {
-                test: /\.styl(us)?$/,
-                use: [
-                    (env === 'development' ? 'style-loader' : {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: '../'
-                        }
-                    }),
-                    'css-loader',
-                    'postcss-loader',
-                    'stylus-loader',
-                ],
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    (env === 'development' ? 'style-loader' : {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: '../'
-                        }
-                    }),
-                    'css-loader',
-                    'postcss-loader',
-                    'less-loader',
-                ],
-            },
-            {
-                test: /\.(sa|sc)ss$/,
-                use: [
-                    (env === 'development' ? 'style-loader' : {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: '../'
-                        }
-                    }),
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 10000,
-                    name: 'images/[name].[ext]',
-
-                },
-            },
-            {
-                test: /\.(mp4|webm|ogg|mp3|wav|flac|aac|m4a)(\?.*)?$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 10000,
-                    name: 'media/[name].[ext]',
-
-                },
-            },
-            {
-                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 10000,
-                    name: 'fonts/[name].[ext]',
-
-                },
-            },
-            {
-                test: /\.(graphql|gql)$/,
-                exclude: /node_modules/,
-                loader: 'graphql-tag/loader'
-            },
-        ],
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(env),
-            'process.env.TARGET': JSON.stringify(target),
-            "process.env.API_HOSTNAME": JSON.stringify(process.env.API_HOSTNAME),
-            "process.env.APP_HOSTNAME": JSON.stringify(process.env.APP_HOSTNAME),
-            "process.env.PROTOCOL": JSON.stringify(process.env.PROTOCOL),
-        }),
-
-        ...(env === 'production' ? [
-            new OptimizeCSSPlugin({
-                cssProcessorOptions: {
-                    safe: true,
-                    map: { inline: false },
-                },
-            }),
-            new webpack.optimize.ModuleConcatenationPlugin(),
-        ] : [
-            // Development only plugins
-            new webpack.HotModuleReplacementPlugin(),
-            new webpack.NamedModulesPlugin(),
-        ]),
-        new HtmlWebpackPlugin({
-            filename: './index.html',
-            template: './src/index.html',
-            inject: true,
-            minify: env === 'production' ? {
-                collapseWhitespace: true,
-                removeComments: true,
-                removeRedundantAttributes: true,
-                removeScriptTypeAttributes: true,
-                removeStyleLinkTypeAttributes: true,
-                useShortDoctype: true
-            } : false,
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].css',
-        }),
-        new CopyWebpackPlugin({
-            patterns: [{
-                    noErrorOnMissing: true,
-                    from: resolvePath('src/static'),
-                    to: resolvePath(isCordova ? 'cordova/www/static' : 'www/static'),
-                },
-                {
-                    noErrorOnMissing: true,
-                    from: resolvePath('src/manifest.json'),
-                    to: resolvePath('www/manifest.json'),
-                },
-            ],
-        }),
-        // ...(!isCordova ? [
-            // new WorkboxPlugin.InjectManifest({
-            //     swSrc: resolvePath('src/OneSignalSDKWorker.js'),
-            // }),
-        // ] : []),
-
+      }),
     ],
+    splitChunks: {
+      chunks: "async",
+      minSize: 30000,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: "~",
+      automaticNameMaxLength: 30,
+      name: true,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(mjs|js|jsx)$/,
+        use: "babel-loader",
+        include: [
+          resolvePath("src"),
+          resolvePath("node_modules/framework7"),
+
+          resolvePath("node_modules/framework7-react"),
+
+          resolvePath("node_modules/template7"),
+          resolvePath("node_modules/dom7"),
+          resolvePath("node_modules/ssr-window"),
+        ],
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          env === "development"
+            ? "style-loader"
+            : {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                  publicPath: "../",
+                },
+              },
+          "css-loader",
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.styl(us)?$/,
+        use: [
+          env === "development"
+            ? "style-loader"
+            : {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                  publicPath: "../",
+                },
+              },
+          "css-loader",
+          "postcss-loader",
+          "stylus-loader",
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          env === "development"
+            ? "style-loader"
+            : {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                  publicPath: "../",
+                },
+              },
+          "css-loader",
+          "postcss-loader",
+          "less-loader",
+        ],
+      },
+      {
+        test: /\.(sa|sc)ss$/,
+        use: [
+          env === "development"
+            ? "style-loader"
+            : {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                  publicPath: "../",
+                },
+              },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "images/[name].[ext]",
+        },
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac|m4a)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "media/[name].[ext]",
+        },
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "fonts/[name].[ext]",
+        },
+      },
+      {
+        test: /\.(graphql|gql)$/,
+        exclude: /node_modules/,
+        loader: "graphql-tag/loader",
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(env),
+      "process.env.TARGET": JSON.stringify(target),
+      "process.env.API_HOSTNAME": JSON.stringify(process.env.API_HOSTNAME),
+      "process.env.APP_HOSTNAME": JSON.stringify(process.env.APP_HOSTNAME),
+      "process.env.PROTOCOL": JSON.stringify(process.env.PROTOCOL),
+    }),
+   
+
+    ...(env === "production"
+      ? [
+          new OptimizeCSSPlugin({
+            cssProcessorOptions: {
+              safe: true,
+              map: { inline: false },
+            },
+          }),
+          new webpack.optimize.ModuleConcatenationPlugin(),
+        ]
+      : [
+          // Development only plugins
+          new webpack.HotModuleReplacementPlugin(),
+          new webpack.NamedModulesPlugin(),
+        ]),
+    new HtmlWebpackPlugin({
+      filename: "./index.html",
+      template: "./src/index.html",
+      inject: true,
+      minify:
+        env === "production"
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+              removeRedundantAttributes: true,
+              removeScriptTypeAttributes: true,
+              removeStyleLinkTypeAttributes: true,
+              useShortDoctype: true,
+            }
+          : false,
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          noErrorOnMissing: true,
+          from: resolvePath("src/static"),
+          to: resolvePath(isCordova ? "cordova/www/static" : "www/static"),
+        },
+        {
+          noErrorOnMissing: true,
+          from: resolvePath("src/manifest.json"),
+          to: resolvePath("www/manifest.json"),
+        },
+      ],
+    }),
+    // ...(!isCordova ? [
+    // new WorkboxPlugin.InjectManifest({
+    //     swSrc: resolvePath('src/OneSignalSDKWorker.js'),
+    // }),
+    // ] : []),
+  ],
 };
