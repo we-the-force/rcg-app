3c41203c8f0dfaaf71137ec4e29a1ebda6589dd7
srlechuga18 <aledleon2@gmail.com>
2021-06-28 15:33:18 -0500

    refect ads

--- src/components/general/ads/ads_feed.jsx
+++ src/components/general/ads/ads_feed.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card, Block } from "framework7-react";
+import { Block } from "framework7-react";
 
 export default class adsFeed extends Component {
 	constructor() {
@@ -9,16 +9,6 @@ export default class adsFeed extends Component {
 	render() {
 		return (
 			<Block className="ads feed">
-				{/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-				<ins
-					className="adsbygoogle"
-					style={{display:"block"}}
-					data-ad-format="fluid"
-					data-ad-layout-key="-6t+ed+2i-1n-4w"
-					data-ad-client="ca-pub-5946382133965347"
-					data-ad-slot="5621731881"
-				></ins>
-				<script>(adsbygoogle = window.adsbygoogle || []).push({});</script> */}
 			</Block>
 		);
 	}
--- src/components/general/ads/ads_in_article.jsx
+++ src/components/general/ads/ads_in_article.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card, Block } from "framework7-react";
+import { Block } from "framework7-react";
 
 export default class adsInArticle extends Component {
 	constructor() {
@@ -9,16 +9,6 @@ export default class adsInArticle extends Component {
 	render() {
 		return (
 			<Block className='adsads'>
-				{/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-				<ins
-					className="adsbygoogle"
-					style={{ display: "block", textAlign: "center" }}
-					data-ad-layout="in-article"
-					data-ad-format="fluid"
-					data-ad-client="ca-pub-5946382133965347"
-					data-ad-slot="7267654347"
-				></ins>
-				<script>(adsbygoogle = window.adsbygoogle || []).push({});</script> */}
 			</Block>
 		);
 	}
