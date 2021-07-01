e63aaa905a0bc69ab88ae8e80d41aedc355c7dc4
Will Alvarez <will.alvarez@gmail.com>
2021-07-01 12:50:28 -0500

    agregar scripts de embed en did mount

--- src/components/articulo/articulo-panel.jsx
+++ src/components/articulo/articulo-panel.jsx
@@ -15,29 +15,32 @@ export default class ArticuloPanel extends Component {
 	constructor(props) {
 		super(props);
 	}
-	componentDidUpdate() {
+	componentDidMount(){
 		window.twttr = (function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0],
-                t = window.twttr || {};
-            if (d.getElementById(id)) return t;
-            js = d.createElement(s);
-            js.id = id;
-            js.src = "https://platform.twitter.com/widgets.js";
-            fjs.parentNode.insertBefore(js, fjs);
+											var js, fjs = d.getElementsByTagName(s)[0],
+															t = window.twttr || {};
+											if (d.getElementById(id)) return t;
+											js = d.createElement(s);
+											js.id = id;
+											js.src = "https://platform.twitter.com/widgets.js";
+											fjs.parentNode.insertBefore(js, fjs);
 
-            t._e = [];
-            t.ready = function(f) {
-                t._e.push(f);
-            };
+											t._e = [];
+											t.ready = function(f) {
+															t._e.push(f);
+											};
 
-            return t;
-        }(document, "script", "twitter-wjs"));
+											return t;
+							}(document, "script", "twitter-wjs"));
 		const script = document.createElement("script");
 
 		script.src = "https://platform.instagram.com/en_US/embeds.js";
 		script.async = true;
 
 		document.body.appendChild(script);
+	}
+	componentDidUpdate() {
+		
 		// FB.XFBML.parse();
 		twttr.widgets.load();
 		window.instgrm.Embeds.process();
