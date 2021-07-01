091aaa163e512d6521522fd010f2ef2688132309
Will Alvarez <will.alvarez@gmail.com>
2021-07-01 12:13:37 -0500

    persistentedQuery apollo client

--- utils/apolloClient.js
+++ utils/apolloClient.js
@@ -1,16 +1,19 @@
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { createPersistedQueryLink } from "@apollo/client/link/persisted-queries";
 import { createHttpLink, HttpLink } from "apollo-link-http";
 
-const cache = new InMemoryCache();
-const link = new HttpLink({
-    // uri: `http://api.rcgmedia.mx/graphql`
-    uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`
-});
+const linkChain = createPersistedQueryLink().concat(
+  new HttpLink({ uri: `${process.env.REACT_APP_BACKEND_URL}/graphql` })
+);
+// const link = new HttpLink({
+//     // uri: `http://api.rcgmedia.mx/graphql`
+//     uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`
+// });
 
 const client = new ApolloClient({
-    cache,
-    link
+    cache: new InMemoryCache(),
+    link: linkChain,
 });
 
 export default client;
